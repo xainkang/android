ef694ca5d7c42d2af03e541e951c2e8bfec54605
xainkang <xainkang@169.com>
2020-07-05 20:33:58 +0800

    完整的代码，代替原来的andriod文件夹

---
+++ app/src/main/java/com/example/sunnyweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.sunnyweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
